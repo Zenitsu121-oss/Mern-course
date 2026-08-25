55d70550e2147861f49889ef538ba0a0b8c7dc9b
Zenitsu121-oss <dineshyt50@gmail.com>
2026-08-25 12:09:35 +0530

    Os node file completed

---
+++ 05.NODE/03.OS/os.js
@@ -0,0 +1,10 @@
+const os=require("os");
+
+// console.log("Os ==>",Os);
+
+
+
+console.log("os type:", os.type());
+console.log("os platform:", os.platform());
+console.log("Arch:", os.arch());
+console.log("cpu:", os.cpus());
